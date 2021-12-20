d702e8dc56eabbe19aa6aa642faa4abe68f0070e
Amit Sharma <95172045+amitsharma-codes@users.noreply.github.com>
2021-12-20 17:23:51 +0530

    Add files via upload

--- occurences.c
+++ occurences.c
@@ -1,17 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    char s1[50],s2[20];int count=0;
+    char s1[50],s2[20],*f;int count=0;
     gets(s1); gets(s2);
+    int len=strlen(s2);
     for(char *p=s1,*q=s2;*p;p++)
     {
-    if(*p==*q)
-    q++;
-    if(*q=='\0')
-    {
-        count++;
-        q=s2;
-    }
+        if(f=strstr(p,q))
+        {
+            p=f;
+            count++;
+        }
     }
     if(count)
     printf("%d\n",count);
